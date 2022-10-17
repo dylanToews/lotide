69f7dc578351525a17d24241ff7d53c97d8118b6
dylanToews <dylan.toews@yahoo.ca>
2022-10-17 14:52:23 -0600

    Changed strings to use Template Literals

--- assertEqual.js
+++ assertEqual.js
@@ -1,8 +1,8 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("✅✅✅" + "Assertation Passed: " + actual + " === " + expected);
+    console.log(`✅✅✅ Assertation Passed: ${actual} === ${expected}`);
   } else if (actual !== expected) {
-    console.log("🛑🛑🛑" + "Assertation Failed: " + actual + " !== " + expected);
+    console.log(`🛑🛑🛑 Assertation Failed: ${actual} !== ${expected}`);
   }
 };
 
