3b3cfe6d22037a6d4b3689f766c83237fb3c24e9
dylanToews <dylan.toews@yahoo.ca>
2022-10-18 17:44:33 -0600

    update on function to compare arrays - changed so it console logged from within function

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,18 +1,24 @@
 const assertArraysEqual = function(array1, array2) {
 
+  let result = '';
+  const failed = `🛑🛑🛑 Assertation Failed: ${array1} !== ${array2}`;
+  const passed = `✅✅✅ Assertation Passed: ${array1} === ${array2}`;
+  
   if (array1.length !== array2.length) {
-    return `🛑🛑🛑 Assertation Failed: ${array1} !== ${array2}`;
+    result = failed;
   } else {
     for (let i = 0; i < array1.length; i++) {
       if (array1[i] !== array2[i]) {
-        return `🛑🛑🛑 Assertation Failed: ${array1} !== ${array2}`;
+        result = failed;
+      } else {
+        result = passed;
       }
-    } return `✅✅✅ Assertation Passed: ${array1} === ${array2}`;
-  }
+    }
+  } console.log(result);
 };
 
 
 
-console.log(assertArraysEqual([1, 2, 3], [1, 2, 3]));
+assertArraysEqual([1, 2, 3,], [1, 2, 3,]);
 
 
