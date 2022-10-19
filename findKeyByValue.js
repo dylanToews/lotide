39537d0069c0a263e28680d8feabd8ada61b902a
dylanToews <dylan.toews@yahoo.ca>
2022-10-19 17:04:18 -0600

    function to find a key based on its value

---
+++ findKeyByValue.js
@@ -0,0 +1,28 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertation Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🛑🛑🛑 Assertation Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire"
+};
+
+const findKeyByValue = function(showObject, nameOfShow) {
+  for (let keys in showObject)
+    if (showObject[keys] === nameOfShow) {
+      return keys;
+    }
+};
+
+console.log(findKeyByValue(bestTVShowsByGenre, "The Expanse"));
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
