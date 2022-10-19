3dfde7edd3ac34121c476233d05ecaa443d0f64a
dylanToews <dylan.toews@yahoo.ca>
2022-10-19 15:48:31 -0600

    function to loop through a string and find the index(s) of each letter in the string

--- letterPosition.js
+++ letterPosition.js
@@ -1,9 +1,49 @@
-const letterPositions = function(sentence) {
-  const results = {};
+const assertArraysEqual = function(array1, array2) {
 
-  // logic of some sort or another 
+  let result;
+  const failed = `🛑🛑🛑 Assertation Failed: ${array1} !== ${array2}`;
+  const passed = `✅✅✅ Assertation Passed: ${array1} === ${array2}`;
 
-  return results; 
-
+  result = eqArrays(array1, array2);
+  console.log(result ? passed : failed);
 };
 
+
+const eqArrays = function(array1, array2) {
+
+  if (array1.length !== array2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+
+const letterPositions = function(sentence) {
+  const results = {};
+  for (let i = 0; i < sentence.length; i++) {
+    let letter = sentence[i];
+    //let emptyArray = []
+    //emptyArray.push(i)
+    if (!results[letter]) {
+      results[letter] = [];
+    }
+
+    results[letter].push(i);
+
+  }
+  return results;
+};
+
+
+console.log(letterPositions("hello"));
+
+assertArraysEqual(letterPositions("hello").e, [1]);
+
+//assertArraysEqual(letterPositions("hello"),{h: [0], e: [1], l: [2, 3], o: [4]})
