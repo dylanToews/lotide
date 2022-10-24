16facff23734f02f00dfb9859544ee1f086bd08b
dylanToews <dylan.toews@yahoo.ca>
2022-10-24 11:51:02 -0600

    continuing to refactor

--- test/tailTest.js
+++ test/tailTest.js
@@ -4,7 +4,3 @@ const tail = (require('../tail'))
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words);
 assertEqual(words.length, 3);
-
-
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result, ["Lighthouse", "Labs"]); // => will always fail!
