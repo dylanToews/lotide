0ed67dae6a3a79f19bba814ae2c15c9783b884bb
dylanToews <dylan.toews@yahoo.ca>
2022-10-24 11:56:29 -0600

    middle function refactored

--- middle.js
+++ middle.js
@@ -1,29 +1,3 @@
-const assertArraysEqual = function(array1, array2) {
-
-  let result;
-  const failed = `🛑🛑🛑 Assertation Failed: ${array1} !== ${array2}`;
-  const passed = `✅✅✅ Assertation Passed: ${array1} === ${array2}`;
-  
-  result = eqArrays(array1, array2);
-  console.log(result ? passed : failed);
-};
-
-
-const eqArrays = function(array1, array2) {
-
-  if (array1.length !== array2.length) {
-    return false;
-  } else {
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] !== array2[i]) {
-        return false;
-      }
-    } return true;
-  }
-};
-
-
-
 const middle = function(array) {
   let answer = [];
   let middleIndex = 0;
@@ -39,10 +13,8 @@ const middle = function(array) {
   }
   return answer;
 };
-  
+
+module.exports = middle
 
 
-
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle(['dog', 'cat', 'sheep', 'pig', 'T-rex']), ['dog']);
 
